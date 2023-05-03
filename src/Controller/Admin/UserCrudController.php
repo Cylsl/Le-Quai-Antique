462458ec027546e0be03f69787ba91c2e981dafa
Cyril Lasselle <lasselle.cyril@gmail.com>
2023-05-03 23:43:18 +0200

    modif fichier

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +16,21 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('lastname', 'Nom'),
+            TextField::new('firstname', 'Prénom'),
+            EmailField::new('email', 'Adresse Email'),
+            ChoiceField::new('roles')
+                ->setLabel('Rôle')
+                ->setChoices([
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ])
+                ->allowMultipleChoices(true)
+                ->renderExpanded(true)
+
         ];
     }
-    */
 }
